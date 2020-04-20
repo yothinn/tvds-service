9da609b1d8bf87e672a64aea068029d56c2477ec
Theerasak Tubrit <theerasaktubrit@Theerasaks-MacBook-Pro.local>
2020-04-20 08:05:26 +0700

    change db server

--- src/config/mongoose.js
+++ src/config/mongoose.js
@@ -3,7 +3,7 @@
 const mongoose = require('mongoose');
 
 module.exports.connection = function (cb) {
-    var MONGODB_URI = process.env.MONGO_DB_URI || process.env.MONGODB_URI || process.env.MONGODB_URI_TEST || "mongodb://35.247.136.98:27017/thamdb"
+    var MONGODB_URI = process.env.MONGO_DB_URI || process.env.MONGODB_URI || process.env.MONGODB_URI_TEST || "mongodb://tvdsowner:covid19%4020@35.244.77.191:27017/thamdata"
     // mongoose.set('debug', process.env.MONGO_DB_URI || process.env.MONGODB_URI ? false : true);
     var db = mongoose.connect(MONGODB_URI,{ useNewUrlParser: true, useUnifiedTopology: true }, function (err) {
         if (err) {
