6f5e2bb3cdb770fb247307145a319afff3a86eaf
Theerasak Tubrit <theerasaktubrit@Theerasaks-MacBook-Pro.local>
2020-04-21 12:26:59 +0700

    integrate socket

--- src/config/socket.io.js
+++ src/config/socket.io.js
@@ -1,18 +1,19 @@
 module.exports.connection = function (http) {
-
-    var io = require('socket.io')(http);
-
-    io.on('connection', function (socket) {
-        console.log('a user connected');
-        socket.on('disconnect', () => {
-            console.log('user disconnected');
-        });
-    
-        socket.on('my message', (msg) => {
-            console.log('message: ' + msg);
-            io.emit('my broadcast', `server: ${msg}`);
-        });
+  var io = require("socket.io")(http);
+  console.log("socket is ready");
+  io.on("connection", function (socket) {
+    console.log("a user connected");
+    socket.on("disconnect", () => {
+      console.log("user disconnected");
     });
 
-    exports.io = io;
-}
+    socket.emit('check-connection', 'connected...');
+
+    socket.on("my message", (msg) => {
+      console.log("message: " + msg);
+      io.emit("my broadcast", `server: ${msg}`);
+    });
+  });
+
+  exports.io = io;
+};
