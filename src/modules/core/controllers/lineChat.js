7a05eaae514b18681d9109e3f9d2569e9ade1dcf
Theerasak Tubrit <theerasaktubrit@Theerasaks-MacBook-Pro.local>
2020-04-25 10:49:49 +0700

    fixed

--- src/modules/core/controllers/lineChat.js
+++ src/modules/core/controllers/lineChat.js
@@ -1,5 +1,5 @@
 const CHANNEL_ACCESS_TOKEN = process.env.CHANNEL_ACCESS_TOKEN || '4A287j6ijUzndwahgTuuucdZTEJOUN/hxlsEX/kjr6WlpPxxxYogNRcx+sZfhfTETeLeLy2jP67GcrOz1AsoJaPkL8t/Pfn+8ZeAYHliSBV3FJNcoVPbodNseuO3sQdAmdxaFFK5TohdUAa3b7UmKQdB04t89/1O/w1cDnyilFU=';
-
+request = require("request");
 module.exports.replyMessage = function (replyToken, messages) {
   let headers = {
     "Content-Type": "application/json",
