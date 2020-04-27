9cb50acaa472b2946261031e624bdfef5796dbad
KrittinTch <petesuper256@gmail.com>
2020-04-27 14:32:06 +0700

    close clg

--- src/modules/vehicledata/test/routes.js
+++ src/modules/vehicledata/test/routes.js
@@ -189,7 +189,6 @@ describe('Vehicledata CRUD routes tests', function () {
                             return done(err);
                         }
                         var resp = res.body;
-                        console.log(resp.data);
                         assert.equal(resp.data.lisenceID, update.lisenceID);
 
                         assert.equal(resp.data.vehicleType, mockup.vehicleType);
